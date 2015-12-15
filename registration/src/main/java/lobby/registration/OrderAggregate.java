84810d070e98ee68b720392f07d7a742bd147b9e
Alexander Litus <alexander.litus@teamdev.com>
2015-12-15 10:30:10 +0200

    Add docs.

--- registration/src/main/java/lobby/registration/OrderAggregate.java
+++ registration/src/main/java/lobby/registration/OrderAggregate.java
@@ -10,6 +10,8 @@ import lobby.registration.order.RejectOrder;
 import lobby.registration.service.OrderPricingService;
 import org.spine3.base.CommandContext;
 import org.spine3.server.Assign;
+import org.spine3.server.Entity;
+import org.spine3.server.EntityId;
 import org.spine3.server.aggregate.Aggregate;
 import org.spine3.server.aggregate.Apply;
 
@@ -26,10 +28,23 @@ public class OrderAggregate extends Aggregate<OrderId, Order> {
 
     private OrderPricingService orderPricingService;
 
+    /**
+     * Creates a new instance.
+     *
+     * @param id the ID for the new instance
+     * @throws IllegalArgumentException if the ID is not of one of the supported types
+     * @see EntityId
+     * @see Entity
+     */
     public OrderAggregate(OrderId id) {
         super(id);
     }
 
+    /**
+     * Sets the pricing service to use to calculate a price of an order.
+     *
+     * @param orderPricingService the pricing service implementation
+     */
     public void setOrderPricingService(OrderPricingService orderPricingService) {
         this.orderPricingService = orderPricingService;
     }
