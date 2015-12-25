fe7165605f17af9a3e2e56375c0302ac99f188a1
Alexander Litus <alexander.litus@teamdev.com>
2015-12-25 15:56:29 +0200

    Add seats availability repository and tests.

---
+++ registration/src/test/java/org/spine3/samples/lobby/registration/seat/availability/SeatsAvailabilityRepositoryShould.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright 2015, TeamDev Ltd. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.spine3.samples.lobby.registration.seat.availability;
+
+import com.google.common.collect.ImmutableList;
+import com.google.protobuf.Message;
+import com.google.protobuf.util.TimeUtil;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.spine3.base.CommandId;
+import org.spine3.base.EventContext;
+import org.spine3.base.EventId;
+import org.spine3.base.EventRecord;
+import org.spine3.samples.lobby.common.ReservationId;
+import org.spine3.samples.lobby.common.SeatTypeId;
+import org.spine3.samples.lobby.registration.contracts.SeatQuantity;
+import org.spine3.server.storage.AggregateStorage;
+import org.spine3.server.storage.memory.InMemoryStorageFactory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.spine3.protobuf.Messages.toAny;
+import static org.spine3.samples.lobby.common.util.CommonMessageFactory.*;
+import static org.spine3.samples.lobby.registration.util.MessageFactory.newSeatQuantity;
+import static org.spine3.samples.lobby.registration.util.MessageFactory.newSeatsAvailabilityId;
+
+/**
+ * @author Alexander Litus
+ */
+@SuppressWarnings({"MagicNumber", "UtilityClass", "InstanceMethodNamingConvention", "RefusedBequest",
+        "ReturnOfCollectionOrArrayField", "TypeMayBeWeakened"})
+public class SeatsAvailabilityRepositoryShould {
+
+    private final SeatsAvailabilityRepository repository = new SeatsAvailabilityRepository();
+
+    @Before
+    public void setUpTest() {
+        final AggregateStorage<SeatsAvailabilityId> storage =
+                InMemoryStorageFactory.getInstance().createAggregateStorage(null);
+        repository.assignStorage(storage);
+    }
+
+    @After
+    public void tearDownTest() {
+        repository.assignStorage(null);
+    }
+
+    @Test
+    public void store_and_load_aggregate() throws InvocationTargetException {
+        final TestSeatsAvailabilityAggregate expected = new TestSeatsAvailabilityAggregate();
+
+        repository.store(expected);
+
+        final SeatsAvailabilityAggregate actual = repository.load(expected.getId());
+        final SeatsAvailability actualState = actual.getState();
+        assertEquals(expected.getAvailableSeats(), actualState.getAvailableSeatList());
+
+        final Map<String, SeatQuantities> pendingReservations = actualState.getPendingReservations();
+        final List<SeatQuantity> reservedSeats = pendingReservations.get(expected.getReservationId()).getItemList();
+        assertEquals(expected.getReservedSeats(), reservedSeats);
+    }
+
+    public static class TestSeatsAvailabilityAggregate extends SeatsAvailabilityAggregate {
+
+        private static final SeatsAvailabilityId ID = newSeatsAvailabilityId();
+
+        private final SeatTypeId seatTypeId = newSeatTypeId();
+
+        private final List<SeatQuantity> availableSeats = ImmutableList.of(newSeatQuantity(seatTypeId, 256));
+
+        private final List<SeatQuantity> reservedSeats = ImmutableList.of(newSeatQuantity(seatTypeId, 128));
+
+        private final ReservationId reservationId = newReservationId();
+
+        private final EventRecord seatsReservedEvent = newEventRecord(SeatsReserved.newBuilder()
+                .setConferenceId(newConferenceId())
+                .setReservationId(reservationId)
+                .addAllReservedSeatUpdated(reservedSeats)
+                .addAllAvailableSeatUpdated(availableSeats).build());
+
+        private final ImmutableList<EventRecord> uncommittedEvents = ImmutableList.of(seatsReservedEvent);
+
+        public TestSeatsAvailabilityAggregate() {
+            super(ID);
+        }
+
+        @Override
+        public List<EventRecord> getUncommittedEvents() {
+            return uncommittedEvents;
+        }
+
+        private static EventRecord newEventRecord(Message event) {
+            final CommandId commandId = CommandId.newBuilder().setTimestamp(TimeUtil.getCurrentTime()).build();
+            final EventId eventId = EventId.newBuilder().setCommandId(commandId).build();
+            final EventContext context = EventContext.newBuilder()
+                    .setAggregateId(toAny(ID))
+                    .setEventId(eventId)
+                    .build();
+            final EventRecord.Builder result = EventRecord.newBuilder()
+                    .setContext(context)
+                    .setEvent(toAny(event));
+            return result.build();
+        }
+
+        public List<SeatQuantity> getAvailableSeats() {
+            return availableSeats;
+        }
+
+        public List<SeatQuantity> getReservedSeats() {
+            return reservedSeats;
+        }
+
+        public String getReservationId() {
+            return reservationId.getUuid();
+        }
+    }
+}
